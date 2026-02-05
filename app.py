8ebe713ef126393c7f7317c48f5655cd6c173d8e
abbaisah41 <abbaisah408@gmail.com>
2026-02-05 18:16:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
+from flask import Flask, request, redirect, session, render_template_string
+import os
+
+app = Flask(__name__)
+app.secret_key = "servwell_secret"
+
 USERNAME = "admin"
 PASSWORD = "2020"
+
+inventory = []
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form.get("username", "").lower()
-        password = request.form.get("password", "")
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         if username == USERNAME and password == PASSWORD:
             session["user"] = username
             return redirect("/")
         else:
-            return render_template_string(HTML_LOGIN + "<p style='color:red;'>Invalid login</p>")
+            return "<h3>Invalid login</h3><a href='/login'>Try again</a>"
 
     return render_template_string(HTML_LOGIN)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/login")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if "user" not in session:
+        return redirect("/login")
+
+    if request.method == "POST":
+        inventory.append({
+            "name": request.form["name"],
+            "qty": request.form["qty"],
+            "price": request.form["price"]
+        })
+
+    return render_template_string(HTML_HOME, inventory=inventory)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 inventory = []
 
